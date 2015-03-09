d75df6039a68f564b3ee8e40a672906f26922d20
Brendan Brown <brendan.brown-1@colorado.edu>
2015-03-09 04:03:48 -0600

    Everythinggg

---
+++ VG_V2/app/src/androidTest/java/androidgraffitiv2/brendan/com/vg_v2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package androidgraffitiv2.brendan.com.vg_v2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ VG_V2/app/src/main/java/androidgraffitiv2/brendan/com/vg_v2/MainActivity.java
@@ -0,0 +1,105 @@
+package androidgraffitiv2.brendan.com.vg_v2;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static String logtag = "VG2App";
+    private static int TAKE_PICTURE = 1;
+    private Uri imageUri;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button cameraButton = (Button)findViewById(R.id.button_camera);
+        //on click listener
+        cameraButton.setOnClickListener(cameraListener);
+
+    }
+
+    private OnClickListener cameraListener = new OnClickListener () {
+        public void onClick(View v) {
+            takePhoto(v);            //takes in view v
+        }
+    };
+
+    private void takePhoto(View v){
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        //make sure user hit OK button
+        if(resultCode== Activity.RESULT_OK) {
+            Uri selectedImage = imageUri;
+            //notify other applications of your content, keeps everyone on the same page
+            getContentResolver().notifyChange(selectedImage, null);
+
+            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ContentResolver cr = getContentResolver();
+            Bitmap bitmap;
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(cr,selectedImage);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            catch(Exception e) {
+                Log.e(logtag, e.toString());
+            }
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
